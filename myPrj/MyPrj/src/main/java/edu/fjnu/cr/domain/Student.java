01c7b3a0f986b13cfaacc5bbde25efdf21928d7b
rui <804050307@qq.com>
2017-06-28 15:08:36 +0800

    myPrj

---
+++ myPrj/MyPrj/src/main/java/edu/fjnu/cr/domain/Student.java
@@ -0,0 +1,19 @@
+package edu.fjnu.cr.domain;
+
+public class Student {
+	private String stuName;
+	private String stuAge;
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	public String getStuAge() {
+		return stuAge;
+	}
+	public void setStuAge(String stuAge) {
+		this.stuAge = stuAge;
+	}
+	
+}
